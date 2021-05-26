a147e1a4b0630a0b5d6a9319646c1329f8ad0a95
VivienCabannes <vivien.cabannes@gmail.com>
2021-05-26 19:02:00 +0200

    Reorganization

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='plasp',
+    version='0.1.0',
+    description='Learning with Partial Supervision',
+    license='MIT',
+    author='Vivien Cabannes',
+    author_email='vivien.cabannes@gmail.com',
+    # packages=[infimum_loss,disambiguation,kernel_laplacian],
+    packages=find_packages(),
+    # url='https://github.com/VivienCabannes/plasp',
+    # download_url='https://github.com/VivienCabannes/plasp/archive/master.zip',
+    # install_requires=[
+    # ],
+    # setup_requires=['pytest-runner'],
+    # tests_require=['pytest'],
+)
