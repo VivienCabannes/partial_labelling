acc0638e9935c0cb93b813e37499074fbd762232
VivienCabannes <vivien.cabannes@gmail.com>
2021-06-05 20:01:26 +0200

    Cleaning and formatting of multilabel

--- plasp/multilabel/variational.py
+++ plasp/multilabel/variational.py
@@ -4,33 +4,30 @@ import numpy as np
 
 
 class IL:
-    def __init__(self, kernel, k):
-        self.kernel = kernel
+    def __init__(self, computer, k):
+        self.computer = computer
         self.k = k
 
-    def train(self, x_train, S_train, lambd):
+    def train(self, x_train, S_train, **kwargs):
         """
         S_train[i,j] = 1 -> i has label j positive
         S_train[i,j] = -1 -> i has label j negative
         S_train[i,j] = 0 -> no information is given for label j of i
         """
-        n_train = len(x_train)
+        self.computer.set_support(x_train)
+        self.computer.train(**kwargs)
         if S_train.dtype == np.bool_:
             phi = np.asfortranarray(S_train, dtype=np.float)
         else:
             phi = S_train
-        self.kernel.set_support(x_train)
-        K_lambda = self.kernel.get_k()
-        K_lambda += lambd * n_train * np.eye(n_train)
-        self.beta = np.linalg.solve(K_lambda, phi)
+        self.computer.set_phi(phi)
 
     def __call__(self, x):
-        K_x = self.kernel(x).T
-        pred = K_x @ self.beta
+        pred = self.computer.call_with_phi(x)
         idx = np.argsort(pred, axis=1)[:, -self.k:]
         pred[:] = -1
         self.fill_topk_pred(pred, idx)
-        return pred    
+        return pred
 
     @staticmethod
     @numba.jit("(f8[:,:], i8[:,:])", nopython=True)
@@ -41,11 +38,31 @@ class IL:
                 pred[i, idx[i, j]] = 1
 
 
-class AC(IL):
-    def __init__(self):
-        super(AC, self).__init__(self)
+if __name__=="__main__":
+    import sys
 
+    sys.path.append('..')
+    from weights import RidgeRegressor, Diffusion
+    from dataloader import MULANLoader, FoldsGenerator
 
-class SP(IL):
-    def __init__(self):
-        super(SP, self).__init__(self)
+    computer = RidgeRegressor('Gaussian', sigma=100)
+    met = IL(computer, k=1)
+
+    loader = MULANLoader('scene')
+    x, y = loader.get_trainset()
+    S = loader.synthetic_corruption(y, .6)
+
+    floader = FoldsGenerator(x, y, S)
+
+    (x, S), (xt, y) = floader()
+    met.train(x, S, lambd=1e-3)
+    y_p = met(xt)
+    print('KRR: ', (y_p == y).mean())
+
+    computer = Diffusion(sigma=10)
+    met = IL(computer, k=1)
+    met.train(x, S, lambd=1e-2, mu=1e-4)
+    y_p = met(xt)
+    print('LAP: ', (y_p == y).mean())
+
+    print('Only negative: ', (y==-1).mean())
